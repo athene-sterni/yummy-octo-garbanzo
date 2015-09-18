e509b34eafc5cfbcef60b8edb287a09640d1e837
Athene Sterni <athene.sterni@gmail.com>
2015-09-18 15:29:56 +0200

    + [SQL] Starting to create views

--- setup.sql
+++ setup.sql
@@ -1,4 +1,8 @@
 -- Drop the tables
+DROP VIEW IF EXISTS vw_discussions;
+
+DROP TABLE IF EXISTS tbl_answers;
+DROP TABLE IF EXISTS tbl_discussions;
 DROP TABLE IF EXISTS tbl_accounts;
 
 -- 
@@ -21,28 +25,44 @@ VALUES (
 	'demo', 'demo@mroman.ch', '0de084f38ace8e3d82597f55cc6ad5d6001568e6',
 	'123', NULL, NOW(), NOW());
 
---
+
 CREATE TABLE tbl_discussions (
 	id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	tag	        VARCHAR(63) NOT NULL,
 	title       VARCHAR(255) NOT NULL,
 	contents    TEXT NOT NULL,
 	author      INT NOT NULL,
+	create_date DATETIME NOT NULL,
 	last_update DATETIME NOT NULL,
 
 	FOREIGN KEY (author) REFERENCES tbl_accounts(id)
 	  ON DELETE CASCADE
 ) ENGINE=INNODB;
 
---
+
 CREATE TABLE tbl_answers (
 	id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	contents    TEXT NOT NULL,
 	author      INT NOT NULL,
     parent      INT,
+    discussion	INT NOT NULL,
+	create_date DATETIME NOT NULL,
+	last_update DATETIME NOT NULL,
 
-    FOREIGN KEY (author) REFERENCES tbl_accounts(id)
+	FOREIGN KEY (author) REFERENCES tbl_accounts(id)
 	  ON DELETE CASCADE,
 	FOREIGN KEY (parent) REFERENCES tbl_answers(id)
-	  ON DELETE CASCADE      
+	  ON DELETE CASCADE,
+	FOREIGN KEY (discussion) REFERENCES tbl_discussions(id)
+	  ON DELETE CASCADE
 ) ENGINE=INNODB;
+
+CREATE VIEW vw_discussions AS
+SELECT
+	tbl_discussions.id as discussion_id, 
+	tbl_accounts.id as account_id, 
+	tbl_accounts.username as author_username 
+FROM tbl_discussions 
+LEFT JOIN tbl_accounts 
+ON tbl_accounts.id = tbl_discussions.author
+ORDER BY tbl_discussions.create_date;
